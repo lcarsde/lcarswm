42b7d2b0a751088118a8d33f8784af66ac29b7a1
Andreas Tennert <mail@andreas-tennert.de>
2019-09-18 20:12:27 +0200

    refactor: move handleConfigureRequest in a separate file

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/eventHandlers.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/eventHandlers.kt
@@ -52,40 +52,6 @@ private fun handleKeyRelease(
     return false
 }
 
-/**
- * Filter the values that lcarswm requires and send the configuration to X.
- */
-private fun handleConfigureRequest(
-    display: CPointer<Display>,
-    windowManagerState: WindowManagerState,
-    xEvent: XEvent
-): Boolean {
-    val configureEvent = xEvent.xconfigurerequest
-
-    println("::handleConfigureRequest::configure request for window ${configureEvent.window}, stack mode: ${configureEvent.detail}, sibling: ${configureEvent.above}, parent: ${configureEvent.parent}")
-
-    val windowChanges = nativeHeap.alloc<XWindowChanges>()
-    windowChanges.x = configureEvent.x
-    windowChanges.y = configureEvent.y
-    windowChanges.width = configureEvent.width
-    windowChanges.height = configureEvent.height
-    windowChanges.sibling = configureEvent.above
-    windowChanges.stack_mode = configureEvent.detail
-    windowChanges.border_width = 0
-
-    if (windowManagerState.hasWindow(configureEvent.window)) {
-        val windowPair = windowManagerState.windows.single {it.first.id == configureEvent.window}
-        val measurements = windowPair.second.getCurrentWindowMeasurements(windowManagerState.getScreenModeForMonitor(windowPair.second))
-
-        val window = windowPair.first
-        sendConfigureNotify(display, window.id, measurements)
-        return false
-    }
-
-    xEventApi().configureWindow(display, configureEvent.window, configureEvent.value_mask.convert(), windowChanges.ptr)
-
-    return false
-}
 
 
 /**
