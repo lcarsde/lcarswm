0b7960ef7defa6b5a9c8e926f1d1146cbc7bc81e
Andreas Tennert <mail@andreas-tennert.de>
2019-09-19 16:36:43 +0200

    refactor: move handleUnmapNotify to separate file

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/eventHandlers.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/eventHandlers.kt
@@ -53,38 +53,6 @@ private fun handleKeyRelease(
 }
 
 
-
-/**
- * Remove the window from the wm data on window unmap.
- */
-private fun handleUnmapNotify(
-    display: CPointer<Display>,
-    windowManagerState: WindowManagerState,
-    xEvent: XEvent,
-    image: CPointer<XImage>,
-    rootWindow: Window,
-    graphicsContexts: List<GC>
-): Boolean {
-    val unmapEvent = xEvent.xunmap
-    println("::handleUnmapNotify::unmapped window: ${unmapEvent.window}")
-    // only the active window can be closed, so make a new window active
-    if (windowManagerState.hasWindow(unmapEvent.window) && unmapEvent.event != rootWindow) {
-        val window = windowManagerState.windows.map { it.first }.single { it.id == unmapEvent.window }
-        xEventApi().unmapWindow(display, window.frame)
-        xEventApi().reparentWindow(display, unmapEvent.window, rootWindow, 0, 0)
-        xWindowUtilApi().removeFromSaveSet(display, unmapEvent.window)
-        xEventApi().destroyWindow(display, window.frame)
-
-        windowManagerState.removeWindow(unmapEvent.window)
-        moveNextWindowToTopOfStack(display, windowManagerState)
-    } else if (windowManagerState.activeWindow != null) {
-        xInputApi().setInputFocus(display, windowManagerState.activeWindow!!.id, RevertToNone, CurrentTime.convert())
-    }
-
-    redrawRootWindow(windowManagerState, graphicsContexts, rootWindow, display, image)
-    return false
-}
-
 private fun loadAppFromKeyBinding(keyBinding: String) {
     val programConfig = readFromConfig(KEY_CONFIG_FILE, keyBinding) ?: return
     println("::loadAppFromKeyBinding::loading app for $keyBinding ${programConfig.size}")
