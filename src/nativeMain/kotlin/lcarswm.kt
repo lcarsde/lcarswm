f37c25faecd2fc018061c3ea3081d2b4a5b4def2
Andreas Tennert <mail@andreas-tennert.de>
2019-10-10 23:13:57 +0200

    fix: uncomment temporarily commented code again

--- src/nativeMain/kotlin/lcarswm.kt
+++ src/nativeMain/kotlin/lcarswm.kt
@@ -112,11 +112,11 @@ fun setupScreen(system: SystemApi, logger: Logger, rootWindow: Window, windowMan
         topLevelWindows.ptr,
         topLevelWindowCount.toCValues())
 
-//    ULongArray(topLevelWindowCount[0].toInt()) {topLevelWindows.value!![it]}
-//        .filter { childId -> childId != rootWindow }
-//        .forEach { childId ->
-//            addWindow(system, logger, windowManagerConfig, rootWindow, childId, true)
-//        }
+    ULongArray(topLevelWindowCount[0].toInt()) {topLevelWindows.value!![it]}
+        .filter { childId -> childId != rootWindow }
+        .forEach { childId ->
+            addWindow(system, logger, windowManagerConfig, rootWindow, childId, true)
+        }
 
     nativeHeap.free(topLevelWindows)
     system.ungrabServer()
@@ -141,7 +141,7 @@ private fun setupRandr(
         return NO_RANDR_BASE
     }
 
-//    handleRandrEvent(system, logger, windowManagerState, image, rootWindow, graphicsContexts)
+    handleRandrEvent(system, logger, windowManagerState, image, rootWindow, graphicsContexts)
 
     system.rSelectInput(rootWindow,
         (RRScreenChangeNotifyMask or
