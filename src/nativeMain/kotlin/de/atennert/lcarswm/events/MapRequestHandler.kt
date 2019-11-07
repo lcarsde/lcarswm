1569a384db1d8c9f739a0394acf3e7c0ec1bf728
Andreas Tennert <mail@andreas-tennert.de>
2019-11-07 17:39:21 +0100

    feat: don't handle known window in MapRequest

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/MapRequestHandler.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/MapRequestHandler.kt
@@ -1,15 +1,20 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.WindowManagerStateHandler
+import de.atennert.lcarswm.system.api.SystemApi
 import xlib.MapRequest
 import xlib.XEvent
 
 /**
  *
  */
-class MapRequestHandler : XEventHandler{
+class MapRequestHandler(
+    private val systemApi: SystemApi,
+    private val windowManagerState: WindowManagerStateHandler
+) : XEventHandler{
     override val xEventType = MapRequest
 
     override fun handleEvent(event: XEvent): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
     }
 }
--- src/nativeTest/kotlin/de/atennert/lcarswm/events/MapRequestHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/MapRequestHandlerTest.kt
@@ -1,14 +1,50 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.Monitor
+import de.atennert.lcarswm.WindowContainer
+import de.atennert.lcarswm.WindowManagerStateMock
+import de.atennert.lcarswm.system.SystemFacadeMock
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.nativeHeap
 import xlib.MapRequest
+import xlib.XEvent
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class MapRequestHandlerTest {
     @Test
     fun `has MapRequest type`() {
-        val mapRequestHandler = MapRequestHandler()
+        val mapRequestHandler = MapRequestHandler(SystemFacadeMock(), WindowManagerStateTestImpl())
 
         assertEquals(MapRequest, mapRequestHandler.xEventType, "The MapRequestHandler should have the type MapRequest")
+    }
+
+    @Test
+    fun `don't handle known windows`() {
+        val system = SystemFacadeMock()
+        val windowManagerState = WindowManagerStateTestImpl()
+
+        val mapRequestHandler = MapRequestHandler(system, windowManagerState)
+
+        val mapRequestEvent = nativeHeap.alloc<XEvent>()
+        mapRequestEvent.type = MapRequest
+        mapRequestEvent.xmaprequest.window = 1.convert()
+
+        val shutdownValue = mapRequestHandler.handleEvent(mapRequestEvent)
+
+        assertEquals(false, shutdownValue, "The MapRequestHandler shouldn't trigger a shutdown")
+
+        assertEquals(0, system.functionCalls.size, "There shouldn't be calls to the outside")
+        assertNull(windowManagerState.addedWindow, "There was no window added to the memory")
+    }
+
+    private class WindowManagerStateTestImpl : WindowManagerStateMock() {
+        var addedWindow: WindowContainer? = null
+
+        override fun addWindow(window: WindowContainer, monitor: Monitor) {
+            addedWindow = window
+        }
     }
 }
