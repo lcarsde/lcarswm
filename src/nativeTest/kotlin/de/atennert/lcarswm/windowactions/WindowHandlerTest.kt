7af864cad914121de6685429bd6d2e0e2c650165
atennert <mail@andreas-tennert.de>
2019-11-19 16:58:52 +0100

    refactor: adjust assertion messages

--- src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/WindowHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/WindowHandlerTest.kt
@@ -239,8 +239,8 @@ class WindowHandlerTest {
         val windowManagerStateCalls = windowManagerState.functionCalls
         
         val unmapFrameCall = unregisterSystemCalls.removeAt(0)
-        assertEquals("unmapWindow", unmapFrameCall.name, "The frame of the unmapped window needs to be _unmapped_")
-        assertEquals(framedWindow.frame, unmapFrameCall.parameters[0], "The _frame_ of the unmapped window needs to be unmapped")
+        assertEquals("unmapWindow", unmapFrameCall.name, "The frame of the removed window needs to be _unmapped_")
+        assertEquals(framedWindow.frame, unmapFrameCall.parameters[0], "The _frame_ of the removed window needs to be unmapped")
 
         val reparentCall = unregisterSystemCalls.removeAt(0)
         assertEquals("reparentWindow", reparentCall.name, "We need to _reparent_ the window back to root")
@@ -252,8 +252,8 @@ class WindowHandlerTest {
         assertEquals(windowId, removeFromSaveSetCall.parameters[0], "We need to remove the _window_ from the save set")
         
         val destroyFrameCall = unregisterSystemCalls.removeAt(0)
-        assertEquals("destroyWindow", destroyFrameCall.name, "The frame of the unmapped window needs to be _destroyed_")
-        assertEquals(framedWindow.frame, destroyFrameCall.parameters[0], "The _frame_ of the unmapped window needs to be destroyed")
+        assertEquals("destroyWindow", destroyFrameCall.name, "The frame of the removed window needs to be _destroyed_")
+        assertEquals(framedWindow.frame, destroyFrameCall.parameters[0], "The _frame_ of the removed window needs to be destroyed")
 
         val removeWindowCall = windowManagerStateCalls.removeAt(0)
         assertEquals("removeWindow", removeWindowCall.name, "The window needs to be _removed_")
