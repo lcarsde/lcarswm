c0df182e9ecd5457306e3a65ff0cf15ae0b7eb44
atennert <mail@andreas-tennert.de>
2019-11-20 16:57:38 +0100

    feat: create RandrScreenChangeHandler

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/RandrHandlerFactory.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/RandrHandlerFactory.kt
@@ -1,4 +1,30 @@
 package de.atennert.lcarswm.events
 
-class RandrHandlerFactory {
+import de.atennert.lcarswm.system.api.RandrApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.pin
+import xlib.RRScreenChangeNotify
+import xlib.XEvent
+
+class RandrHandlerFactory(randrApi: RandrApi) {
+    private val randrEventBase: Int
+    private val randrErrorBase: Int
+
+    init {
+        val eventBase = IntArray(1).pin()
+        val errorBase = IntArray(1).pin()
+
+        randrApi.rQueryExtension(eventBase.addressOf(0), errorBase.addressOf(0))
+
+        randrEventBase = eventBase.get()[0]
+        randrErrorBase = errorBase.get()[0]
+    }
+
+    fun createScreenChangeHandler(): XEventHandler = object : XEventHandler {
+        override val xEventType = randrEventBase + RRScreenChangeNotify
+
+        override fun handleEvent(event: XEvent): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
 }
--- src/nativeTest/kotlin/de/atennert/lcarswm/events/RandrHandlerFactoryTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/RandrHandlerFactoryTest.kt
@@ -1,10 +1,20 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.system.SystemFacadeMock
+import xlib.RRScreenChangeNotify
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class RandrHandlerFactoryTest {
     @Test
     fun `create RandrScreenChangeHandler`() {
-        val randrHandlerFactory = RandrHandlerFactory()
+        val systemApi = SystemFacadeMock()
+        val randrHandlerFactory = RandrHandlerFactory(systemApi)
+
+        val screenChangeHandler = randrHandlerFactory.createScreenChangeHandler()
+
+        assertEquals(systemApi.randrEventBase + RRScreenChangeNotify,
+                screenChangeHandler.xEventType,
+                "The factory should create a screen change handler with the appropriate event type")
     }
 }
