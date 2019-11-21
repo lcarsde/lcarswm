9c1426a7a5058f3c055d3f039f84d6941eb34b75
atennert <mail@andreas-tennert.de>
2019-11-21 21:23:23 +0100

    test: request control resources in monitor manager test

--- src/nativeTest/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImplTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImplTest.kt
@@ -1,10 +1,17 @@
 package de.atennert.lcarswm.monitor
 
+import de.atennert.lcarswm.system.SystemFacadeMock
 import kotlin.test.Test
 
 class MonitorManagerImplTest {
     @Test
     fun `update monitor list`() {
-        val monitorManager: MonitorManager = MonitorManagerImpl()
+        val systemApi = SystemFacadeMock()
+        val monitorManager: MonitorManager = MonitorManagerImpl(systemApi)
+
+        monitorManager.updateMonitorList()
+
+        val monitorList = monitorManager.getMonitors()
+        val primaryMonitor = monitorManager.getPrimaryMonitor()
     }
 }
