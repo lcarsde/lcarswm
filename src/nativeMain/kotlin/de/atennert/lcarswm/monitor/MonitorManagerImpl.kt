567374d22b56cc067ec0071b1b8cf4aeb0f47baf
atennert <mail@andreas-tennert.de>
2019-11-21 20:46:29 +0100

    feat: add MonitorManager implementation

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImpl.kt
@@ -0,0 +1,7 @@
+package de.atennert.lcarswm.monitor
+
+class MonitorManagerImpl : MonitorManager {
+    override fun updateMonitorList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ src/nativeTest/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImplTest.kt
@@ -0,0 +1,10 @@
+package de.atennert.lcarswm.monitor
+
+import kotlin.test.Test
+
+class MonitorManagerImplTest {
+    @Test
+    fun `update monitor list`() {
+        val monitorManager: MonitorManager = MonitorManagerImpl()
+    }
+}
