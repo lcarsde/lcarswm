0236ef9553e951a5c224ef5dd683995464c945dc
atennert <mail@andreas-tennert.de>
2019-11-22 20:30:43 +0100

    feat: update monitor list

--- src/nativeMain/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImpl.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImpl.kt
@@ -1,17 +1,22 @@
 package de.atennert.lcarswm.monitor
 
 import de.atennert.lcarswm.system.api.RandrApi
+import kotlinx.cinterop.get
+import kotlinx.cinterop.pointed
+import xlib.Window
 
-class MonitorManagerImpl(private val randrApi: RandrApi) : MonitorManager {
+class MonitorManagerImpl(private val randrApi: RandrApi, private val rootWindowId: Window) : MonitorManager {
+    private var monitors: List<Monitor> = listOf();
+
     override fun updateMonitorList() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val screenResources = randrApi.rGetScreenResources(rootWindowId)!!.pointed
+        val primaryScreen = randrApi.rGetOutputPrimary(rootWindowId)
+
+        monitors = Array(screenResources.noutput) {screenResources.outputs!![it]}
+                .map { Monitor(it, "", it == primaryScreen) }
     }
 
-    override fun getMonitors(): List<Monitor> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getMonitors(): List<Monitor> = monitors.toList()
 
-    override fun getPrimaryMonitor(): Monitor {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getPrimaryMonitor(): Monitor = monitors.single { it.isPrimary }
 }
--- src/nativeTest/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImplTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/monitor/MonitorManagerImplTest.kt
@@ -2,16 +2,25 @@ package de.atennert.lcarswm.monitor
 
 import de.atennert.lcarswm.system.SystemFacadeMock
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class MonitorManagerImplTest {
     @Test
     fun `update monitor list`() {
         val systemApi = SystemFacadeMock()
-        val monitorManager: MonitorManager = MonitorManagerImpl(systemApi)
+        val monitorManager: MonitorManager = MonitorManagerImpl(systemApi, systemApi.rootWindowId)
 
         monitorManager.updateMonitorList()
 
         val monitorList = monitorManager.getMonitors()
         val primaryMonitor = monitorManager.getPrimaryMonitor()
+
+        assertEquals(systemApi.outputs.size, monitorList.size, "There should be as many monitors as provided outputs")
+
+        assertTrue(monitorList.contains(primaryMonitor), "The primary monitor should be part of the monitor list")
+        assertEquals(systemApi.primaryOutput, primaryMonitor.id, "The ID of the primary monitor should match")
     }
+
+    // TODO check monitor update for no provided primary
 }
