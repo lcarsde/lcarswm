f4217d47cab2c18a35dc0f5e03976cdfe09610dc
Andreas Tennert <mail@andreas-tennert.de>
2019-11-27 20:47:56 +0100

    test: resize the root window on screen change

--- src/nativeTest/kotlin/de/atennert/lcarswm/events/RandrScreenChangeHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/RandrScreenChangeHandlerTest.kt
@@ -5,6 +5,7 @@ import de.atennert.lcarswm.log.LoggerMock
 import de.atennert.lcarswm.monitor.MonitorManagerMock
 import de.atennert.lcarswm.system.SystemFacadeMock
 import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
 import kotlinx.cinterop.nativeHeap
 import xlib.RRScreenChangeNotify
 import xlib.XEvent
@@ -46,6 +47,12 @@ class RandrScreenChangeHandlerTest {
 
         val updateMonitorListCall = monitorManager.functionCalls.removeAt(0)
         assertEquals("updateMonitorList", updateMonitorListCall.name, "The monitor list needs to be updated")
+
+        val resizeRootWindowCall = systemApi.functionCalls.removeAt(0)
+        assertEquals("resizeWindow", resizeRootWindowCall.name, "The root window needs to be _resized_ to the new combined screen measurement")
+        assertEquals(systemApi.rootWindowId, resizeRootWindowCall.parameters[0], "The _root window_ needs to be resized to the new combined screen measurement")
+        assertEquals(2000.toUInt(), resizeRootWindowCall.parameters[1], "The root window needs to be resized to new _screen width_")
+        assertEquals(500.toUInt(), resizeRootWindowCall.parameters[2], "The root window needs to be resized to new _screen height_")
 
         val redrawUiCall = uiDrawer.functionCalls.removeAt(0)
         assertEquals("drawWindowManagerFrame", redrawUiCall.name, "The window manager UI needs to be redrawn on the updated monitors")
