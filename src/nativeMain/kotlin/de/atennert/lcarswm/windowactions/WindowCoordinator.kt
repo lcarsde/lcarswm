1cf30c06a8d9f333f2a37735994fe173680bbf20
Andreas Tennert <mail@andreas-tennert.de>
2019-11-29 19:23:43 +0100

    feat: allow adding windows to monitors

--- src/nativeMain/kotlin/de/atennert/lcarswm/windowactions/WindowCoordinator.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/windowactions/WindowCoordinator.kt
@@ -1,5 +1,9 @@
 package de.atennert.lcarswm.windowactions
 
+import xlib.Window
+
 interface WindowCoordinator {
     fun rearrangeActiveWindows()
+
+    fun addWindowToMonitor(windowId: Window)
 }
--- src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/WindowCoordinatorMock.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/WindowCoordinatorMock.kt
@@ -1,11 +1,16 @@
 package de.atennert.lcarswm.windowactions
 
 import de.atennert.lcarswm.system.FunctionCall
+import xlib.Window
 
 class WindowCoordinatorMock : WindowCoordinator {
     val functionCalls = mutableListOf<FunctionCall>()
 
     override fun rearrangeActiveWindows() {
         functionCalls.add(FunctionCall("rearrangeActiveWindows"))
+    }
+
+    override fun addWindowToMonitor(windowId: Window) {
+        functionCalls.add(FunctionCall("addWindowToMonitor", windowId))
     }
 }
