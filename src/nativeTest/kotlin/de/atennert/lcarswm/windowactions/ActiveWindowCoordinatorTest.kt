784e51bf3d8892b7fa849f0d969949d688aa1774
Andreas Tennert <mail@andreas-tennert.de>
2019-12-02 18:16:06 +0100

    test: remove window from window coordinator

--- src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/ActiveWindowCoordinatorTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/windowactions/ActiveWindowCoordinatorTest.kt
@@ -4,6 +4,7 @@ import de.atennert.lcarswm.monitor.MonitorManagerMock
 import de.atennert.lcarswm.system.SystemFacadeMock
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 class ActiveWindowCoordinatorTest {
     @Test
@@ -25,5 +26,20 @@ class ActiveWindowCoordinatorTest {
             monitorManager.primaryMonitor, activeWindowCoordinator.getMonitorForWindow(windowId),
             "The monitor manager should have stored the window-monitor-relation"
         )
+    }
+
+    @Test
+    fun `remove window from coordinator`() {
+        val systemApi = SystemFacadeMock()
+        val windowId = systemApi.getNewWindowId()
+        val monitorManager = MonitorManagerMock()
+
+        val activeWindowCoordinator = ActiveWindowCoordinator(monitorManager)
+
+        activeWindowCoordinator.addWindowToMonitor(windowId)
+
+        activeWindowCoordinator.removeWindow(windowId)
+
+        assertFails("The window should be removed") { activeWindowCoordinator.getMonitorForWindow(windowId) }
     }
 }
