d5db839eeacb8a22299b8caa57838d57d8b1e772
Andreas Tennert <mail@andreas-tennert.de>
2019-12-11 20:18:15 +0100

    test: grab modifier keys for root window

--- src/nativeTest/kotlin/de/atennert/lcarswm/KeyManagerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/KeyManagerTest.kt
@@ -1,6 +1,7 @@
 package de.atennert.lcarswm
 
 import de.atennert.lcarswm.system.SystemFacadeMock
+import xlib.AnyModifier
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -15,5 +16,22 @@ class KeyManagerTest {
             listOf(systemApi.modifiers[6]), keyManager.modifiers,
             "The KeyManager should get the required modifier keys"
         )
+    }
+
+    @Test
+    fun `grab input controls`() {
+        val systemApi = SystemFacadeMock()
+
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+
+        keyManager.grabInputControls()
+
+        val inputCalls = systemApi.functionCalls
+
+        val grabModifierKeyCall = inputCalls.removeAt(0)
+        assertEquals("grabKey", grabModifierKeyCall.name, "The modifier key needs to be grabbed")
+        assertEquals(systemApi.modifiers[6].toInt(), grabModifierKeyCall.parameters[0], "The modifier key needs to be grabbed")
+        assertEquals(AnyModifier.toUInt(), grabModifierKeyCall.parameters[1], "The modifier modifier key needs to be any")
+        assertEquals(systemApi.rootWindowId, grabModifierKeyCall.parameters[2], "The modifier key needs to be grabbed for the root window")
     }
 }
