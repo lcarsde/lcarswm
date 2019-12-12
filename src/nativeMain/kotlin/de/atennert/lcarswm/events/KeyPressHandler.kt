afefb5cca2ba2b6221affe283730b71dcbf2227c
Andreas Tennert <mail@andreas-tennert.de>
2019-12-12 19:53:03 +0100

    feat: redraw WM window when moving the active window

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyPressHandler.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyPressHandler.kt
@@ -1,12 +1,20 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.KeyManager
+import de.atennert.lcarswm.UIDrawing
+import de.atennert.lcarswm.windowactions.WindowCoordinator
 import xlib.KeyPress
 import xlib.XEvent
 
-class KeyPressHandler : XEventHandler {
+class KeyPressHandler(
+    private val keyManager: KeyManager,
+    private val windowCoordinator: WindowCoordinator,
+    private val uiDrawer: UIDrawing
+) : XEventHandler {
     override val xEventType = KeyPress
 
     override fun handleEvent(event: XEvent): Boolean {
-        TODO()
+        uiDrawer.drawWindowManagerFrame()
+        return false
     }
 }
