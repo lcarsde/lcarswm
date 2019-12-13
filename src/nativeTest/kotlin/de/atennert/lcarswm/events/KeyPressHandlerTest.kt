003d3b07e00a16095f9d34715d529102445327b9
Andreas Tennert <mail@andreas-tennert.de>
2019-12-13 22:43:56 +0100

    test: move the currently focused window to the next monitor

--- src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyPressHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyPressHandlerTest.kt
@@ -4,6 +4,7 @@ import de.atennert.lcarswm.KeyManager
 import de.atennert.lcarswm.UIDrawingMock
 import de.atennert.lcarswm.system.SystemFacadeMock
 import de.atennert.lcarswm.windowactions.WindowCoordinatorMock
+import de.atennert.lcarswm.windowactions.WindowFocusHandler
 import kotlinx.cinterop.alloc
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.nativeHeap
@@ -20,8 +21,9 @@ class KeyPressHandlerTest {
         val systemApi = SystemFacadeMock()
         val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
         val windowCoordinator = WindowCoordinatorMock()
+        val windowFocusHandler = WindowFocusHandler()
         val uiDrawing = UIDrawingMock()
-        val keyPressHandler = KeyPressHandler(keyManager, windowCoordinator, uiDrawing)
+        val keyPressHandler = KeyPressHandler(keyManager, windowCoordinator, windowFocusHandler, uiDrawing)
 
         assertEquals(KeyPress, keyPressHandler.xEventType, "The key press handler should have the correct type")
     }
@@ -31,10 +33,12 @@ class KeyPressHandlerTest {
         val systemApi = SystemFacadeMock()
         val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
         val windowCoordinator = WindowCoordinatorMock()
+        val windowFocusHandler = WindowFocusHandler()
         val uiDrawer = UIDrawingMock()
         keyManager.grabInputControls()
+        windowFocusHandler.setFocusedWindow(systemApi.getNewWindowId())
 
-        val keyPressHandler = KeyPressHandler(keyManager, windowCoordinator, uiDrawer)
+        val keyPressHandler = KeyPressHandler(keyManager, windowCoordinator, windowFocusHandler, uiDrawer)
 
         val keyPressEvent = nativeHeap.alloc<XEvent>()
         keyPressEvent.type = KeyPress
@@ -44,7 +48,9 @@ class KeyPressHandlerTest {
 
         assertFalse(shutdownValue, "Handling the up-key shouldn't trigger a shutdown")
 
-        // TODO redraw active window
+        val moveWindowCall = windowCoordinator.functionCalls.removeAt(0)
+        assertEquals("moveWindowToNextMonitor", moveWindowCall.name, "The focused window should be moved to the next monitor")
+        assertEquals(windowFocusHandler.getFocusedWindow(), moveWindowCall.parameters[0], "The _focused window_ should be moved to the next monitor")
 
         val uiRedrawCall = uiDrawer.functionCalls.removeAt(0)
         assertEquals("drawWindowManagerFrame", uiRedrawCall.name, "The WM UI needs to be redrawn")
