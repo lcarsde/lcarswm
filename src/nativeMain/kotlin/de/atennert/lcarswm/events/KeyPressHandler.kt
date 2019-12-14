5cb21d01996d7604f21a5036fc628a8e408152b5
Andreas Tennert <mail@andreas-tennert.de>
2019-12-14 20:12:57 +0100

    feat: move the currently focused window to the previous monitor

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyPressHandler.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyPressHandler.kt
@@ -4,8 +4,11 @@ import de.atennert.lcarswm.KeyManager
 import de.atennert.lcarswm.UIDrawing
 import de.atennert.lcarswm.windowactions.WindowCoordinator
 import de.atennert.lcarswm.windowactions.WindowFocusHandler
+import kotlinx.cinterop.convert
 import xlib.KeyPress
 import xlib.XEvent
+import xlib.XK_Down
+import xlib.XK_Up
 
 class KeyPressHandler(
     private val keyManager: KeyManager,
@@ -16,8 +19,18 @@ class KeyPressHandler(
     override val xEventType = KeyPress
 
     override fun handleEvent(event: XEvent): Boolean {
-        windowCoordinator.moveWindowToNextMonitor(windowFocusHandler.getFocusedWindow()!!)
-        uiDrawer.drawWindowManagerFrame()
+        val keyCode = event.xkey.keycode
+        when (keyManager.getKeySym(keyCode.convert()).convert<Int>()) {
+            XK_Up -> {
+                windowCoordinator.moveWindowToNextMonitor(windowFocusHandler.getFocusedWindow()!!)
+                uiDrawer.drawWindowManagerFrame()
+            }
+            XK_Down -> {
+                windowCoordinator.moveWindowToPreviousMonitor(windowFocusHandler.getFocusedWindow()!!)
+                uiDrawer.drawWindowManagerFrame()
+            }
+        }
+
         return false
     }
 }
