7154caa4e69f3e9a95369cfc669237484d716348
Andreas Tennert <mail@andreas-tennert.de>
2019-12-14 20:12:43 +0100

    test: move the currently focused window to the previous monitor

--- src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyPressHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyPressHandlerTest.kt
@@ -10,6 +10,7 @@ import kotlinx.cinterop.convert
 import kotlinx.cinterop.nativeHeap
 import xlib.KeyPress
 import xlib.XEvent
+import xlib.XK_Down
 import xlib.XK_Up
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -51,6 +52,34 @@ class KeyPressHandlerTest {
         val moveWindowCall = windowCoordinator.functionCalls.removeAt(0)
         assertEquals("moveWindowToNextMonitor", moveWindowCall.name, "The focused window should be moved to the next monitor")
         assertEquals(windowFocusHandler.getFocusedWindow(), moveWindowCall.parameters[0], "The _focused window_ should be moved to the next monitor")
+
+        val uiRedrawCall = uiDrawer.functionCalls.removeAt(0)
+        assertEquals("drawWindowManagerFrame", uiRedrawCall.name, "The WM UI needs to be redrawn")
+    }
+
+    @Test
+    fun `move active window to the previous monitor`() {
+        val systemApi = SystemFacadeMock()
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+        val windowCoordinator = WindowCoordinatorMock()
+        val windowFocusHandler = WindowFocusHandler()
+        val uiDrawer = UIDrawingMock()
+        keyManager.grabInputControls()
+        windowFocusHandler.setFocusedWindow(systemApi.getNewWindowId())
+
+        val keyPressHandler = KeyPressHandler(keyManager, windowCoordinator, windowFocusHandler, uiDrawer)
+
+        val keyPressEvent = nativeHeap.alloc<XEvent>()
+        keyPressEvent.type = KeyPress
+        keyPressEvent.xkey.keycode = systemApi.keySyms.getValue(XK_Down).convert()
+
+        val shutdownValue = keyPressHandler.handleEvent(keyPressEvent)
+
+        assertFalse(shutdownValue, "Handling the up-key shouldn't trigger a shutdown")
+
+        val moveWindowCall = windowCoordinator.functionCalls.removeAt(0)
+        assertEquals("moveWindowToPreviousMonitor", moveWindowCall.name, "The focused window should be moved to the previous monitor")
+        assertEquals(windowFocusHandler.getFocusedWindow(), moveWindowCall.parameters[0], "The _focused window_ should be moved to the previous monitor")
 
         val uiRedrawCall = uiDrawer.functionCalls.removeAt(0)
         assertEquals("drawWindowManagerFrame", uiRedrawCall.name, "The WM UI needs to be redrawn")
