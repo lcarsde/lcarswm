87a81cf4cd73506ab78b086357837a9ccf2a7613
Andreas Tennert <mail@andreas-tennert.de>
2019-12-24 10:02:27 +0100

    test: add shutdown on releasing Q

--- src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyReleaseHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyReleaseHandlerTest.kt
@@ -1,8 +1,16 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.system.SystemFacadeMock
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.nativeHeap
+import kotlinx.cinterop.pointed
 import xlib.KeyRelease
+import xlib.XEvent
+import xlib.XK_Q
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  *
@@ -13,5 +21,21 @@ class KeyReleaseHandlerTest {
         val keyReleaseHandler = KeyReleaseHandler()
 
         assertEquals(KeyRelease, keyReleaseHandler.xEventType, "The type of the key release handler should be KeyRelease")
+    }
+
+    @Test
+    fun `shutdown on Q`() {
+        val systemApi = SystemFacadeMock()
+
+        val event = nativeHeap.alloc<XEvent>()
+        event.type = KeyRelease
+        event.xkey.keycode = systemApi.keySyms.getValue(XK_Q).convert()
+        event.xkey.state = systemApi.modifiers[systemApi.winModifierPosition].convert()
+
+        val keyReleaseHandler = KeyReleaseHandler()
+
+        val shutdownValue = keyReleaseHandler.handleEvent(event)
+
+        assertTrue(shutdownValue, "The window manager should shut down on Q")
     }
 }
