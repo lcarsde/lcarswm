eebf3274216ef078577a9f0f4ca50a1079eefe47
Andreas Tennert <mail@andreas-tennert.de>
2019-12-25 20:34:31 +0100

    test: send kill window on F4 with bad wm protocols result

--- src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyReleaseHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/KeyReleaseHandlerTest.kt
@@ -1,14 +1,13 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.KeyManager
 import de.atennert.lcarswm.system.SystemFacadeMock
-import kotlinx.cinterop.alloc
-import kotlinx.cinterop.convert
-import kotlinx.cinterop.nativeHeap
-import xlib.KeyRelease
-import xlib.XEvent
-import xlib.XK_Q
+import de.atennert.lcarswm.windowactions.WindowFocusHandler
+import kotlinx.cinterop.*
+import xlib.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 /**
@@ -17,24 +16,70 @@ import kotlin.test.assertTrue
 class KeyReleaseHandlerTest {
     @Test
     fun `return the event type KeyReleaseHandler`() {
-        val keyReleaseHandler = KeyReleaseHandler()
+        val systemApi = SystemFacadeMock()
+        val focusHandler = WindowFocusHandler()
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
 
-        assertEquals(KeyRelease, keyReleaseHandler.xEventType, "The type of the key release handler should be KeyRelease")
+        val keyReleaseHandler = KeyReleaseHandler(systemApi, focusHandler, keyManager)
+
+        assertEquals(
+            KeyRelease,
+            keyReleaseHandler.xEventType,
+            "The type of the key release handler should be KeyRelease"
+        )
     }
 
     @Test
     fun `shutdown on Q`() {
         val systemApi = SystemFacadeMock()
+        val focusHandler = WindowFocusHandler()
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+        keyManager.grabInputControls()
+
+        systemApi.functionCalls.clear()
 
         val keyReleaseEvent = nativeHeap.alloc<XEvent>()
         keyReleaseEvent.type = KeyRelease
         keyReleaseEvent.xkey.keycode = systemApi.keySyms.getValue(XK_Q).convert()
         keyReleaseEvent.xkey.state = systemApi.modifiers[systemApi.winModifierPosition].convert()
 
-        val keyReleaseHandler = KeyReleaseHandler()
+        val keyReleaseHandler = KeyReleaseHandler(systemApi, focusHandler, keyManager)
 
         val shutdownValue = keyReleaseHandler.handleEvent(keyReleaseEvent)
 
         assertTrue(shutdownValue, "The window manager should shut down on Q")
+    }
+
+    @Test
+    fun `kill active window if the WM protocols result is != 0 on F4`() {
+        val systemApi = object : SystemFacadeMock() {
+            override fun getWMProtocols(
+                window: Window,
+                protocolsReturn: CValuesRef<CPointerVar<AtomVar>>,
+                protocolCountReturn: CValuesRef<IntVar>
+            ): Int = -1
+        }
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+        val focusHandler = WindowFocusHandler()
+        val windowId = systemApi.getNewWindowId()
+        focusHandler.setFocusedWindow(windowId)
+        keyManager.grabInputControls()
+
+        systemApi.functionCalls.clear()
+
+        val keyReleaseEvent = nativeHeap.alloc<XEvent>()
+        keyReleaseEvent.type = KeyRelease
+        keyReleaseEvent.xkey.keycode = systemApi.keySyms.getValue(XK_F4).convert()
+        keyReleaseEvent.xkey.state = systemApi.modifiers[systemApi.winModifierPosition].convert()
+
+        val keyReleaseHandler = KeyReleaseHandler(systemApi, focusHandler, keyManager)
+
+        val shutdownValue = keyReleaseHandler.handleEvent(keyReleaseEvent)
+
+        assertFalse(shutdownValue, "The window manager should not shut down on F4")
+
+        val killClientCall = systemApi.functionCalls.removeAt(0)
+        assertEquals("killClient", killClientCall.name, "The window should be killed if we can't get its protocols")
+        assertEquals(windowId, killClientCall.parameters[0], "The active window needs to be killed")
     }
 }
