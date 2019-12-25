6bcb17fdfb3b7955b9ed16da39fabd90076757b6
Andreas Tennert <mail@andreas-tennert.de>
2019-12-25 20:34:56 +0100

    feat: send kill window on F4 with bad wm protocols result

--- src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyReleaseHandler.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/KeyReleaseHandler.kt
@@ -1,15 +1,36 @@
 package de.atennert.lcarswm.events
 
+import de.atennert.lcarswm.KeyManager
+import de.atennert.lcarswm.system.api.SystemApi
+import de.atennert.lcarswm.windowactions.WindowFocusHandler
+import kotlinx.cinterop.convert
 import xlib.KeyRelease
 import xlib.XEvent
+import xlib.XK_F4
+import xlib.XK_Q
 
 /**
  *
  */
-class KeyReleaseHandler : XEventHandler {
+class KeyReleaseHandler(
+    private val systemApi: SystemApi,
+    private val focusHandler: WindowFocusHandler,
+    private val keyManager: KeyManager
+) :
+    XEventHandler {
     override val xEventType = KeyRelease
 
     override fun handleEvent(event: XEvent): Boolean {
-        return true
+        val keyCode = event.xkey.keycode
+        when (keyManager.getKeySym(keyCode.convert()).convert<Int>()) {
+            XK_F4 -> {
+                val focusedWindow = focusHandler.getFocusedWindow()
+                if (focusedWindow != null) {
+                    systemApi.killClient(focusedWindow)
+                }
+            }
+            XK_Q -> return true
+        }
+        return false
     }
 }
