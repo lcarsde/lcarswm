3f1a877ad61eb4e1dcde8fe753977394a8d4af81
Andreas Tennert <mail@andreas-tennert.de>
2020-01-02 16:22:41 +0100

    refactor: clean up byte array use in configuration provider

--- src/nativeMain/kotlin/de/atennert/lcarswm/ConfigurationProvider.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/ConfigurationProvider.kt
@@ -2,9 +2,11 @@ package de.atennert.lcarswm
 
 import de.atennert.lcarswm.system.api.PosixApi
 import kotlinx.cinterop.addressOf
-import kotlinx.cinterop.pin
+import kotlinx.cinterop.usePinned
 
 class ConfigurationProvider(posixApi: PosixApi, configurationFilePath: String) {
+
+    private val readBufferSize = 60
 
     private val properties: Map<String, String>
 
@@ -14,17 +16,18 @@ class ConfigurationProvider(posixApi: PosixApi, configurationFilePath: String) {
         properties = if (filePointer == null) {
             emptyMap()
         } else {
-            val entry = ByteArray(60).pin()
             val entryMap = mutableMapOf<String, String>()
 
-            while (posixApi.fgets(entry.addressOf(0), entry.get().size, filePointer) != null) {
-                val entryPair = entry.get()
-                    .takeWhile { it > 0 }
-                    .fold("") {acc, b -> acc + b.toChar()}
-                    .trim()
-                    .split('=')
+            ByteArray(readBufferSize).usePinned { entry ->
+                while (posixApi.fgets(entry.addressOf(0), readBufferSize, filePointer) != null) {
+                    val entryPair = entry.get()
+                        .takeWhile { it > 0 }
+                        .fold("") {acc, b -> acc + b.toChar()}
+                        .trim()
+                        .split('=')
 
-                entryMap[entryPair[0]] = entryPair[1]
+                    entryMap[entryPair[0]] = entryPair[1]
+                }
             }
 
             posixApi.fclose(filePointer)
