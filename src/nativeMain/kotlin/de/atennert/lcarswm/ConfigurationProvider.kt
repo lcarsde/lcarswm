a931400b195c61e87550d1e86647a72755c56c7f
Andreas Tennert <mail@andreas-tennert.de>
2020-01-02 16:13:25 +0100

    feat: load properties

--- src/nativeMain/kotlin/de/atennert/lcarswm/ConfigurationProvider.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/ConfigurationProvider.kt
@@ -1,10 +1,36 @@
 package de.atennert.lcarswm
 
 import de.atennert.lcarswm.system.api.PosixApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.pin
 
 class ConfigurationProvider(posixApi: PosixApi, configurationFilePath: String) {
 
-    operator fun get(propertyKey: String): String? {
-        return ""
+    private val properties: Map<String, String>
+
+    init {
+        val filePointer = posixApi.fopen(configurationFilePath, "r")
+
+        properties = if (filePointer == null) {
+            emptyMap()
+        } else {
+            val entry = ByteArray(60).pin()
+            val entryMap = mutableMapOf<String, String>()
+
+            while (posixApi.fgets(entry.addressOf(0), entry.get().size, filePointer) != null) {
+                val entryPair = entry.get()
+                    .takeWhile { it > 0 }
+                    .fold("") {acc, b -> acc + b.toChar()}
+                    .trim()
+                    .split('=')
+
+                entryMap[entryPair[0]] = entryPair[1]
+            }
+
+            posixApi.fclose(filePointer)
+            entryMap
+        }
     }
+
+    operator fun get(propertyKey: String): String? = properties[propertyKey]
 }
