4a7e1ec15f52ad444b558e51591c6f8c293134a6
Andreas Tennert <mail@andreas-tennert.de>
2020-01-07 20:08:52 +0100

    feat: load key config which has properties with up to one modifier

--- src/nativeMain/kotlin/de/atennert/lcarswm/KeyConfiguration.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/KeyConfiguration.kt
@@ -1,20 +1,50 @@
 package de.atennert.lcarswm
 
 import de.atennert.lcarswm.system.api.InputApi
+import kotlinx.cinterop.convert
 import xlib.KeySym
 
-class KeyConfiguration(inputApi: InputApi, configurationProvider: Properties) {
+class KeyConfiguration(inputApi: InputApi, configurationProvider: Properties, private val keyManager: KeyManager) {
 
-    private val keySymCommands = mutableMapOf<KeySym, String>()
+    private val keySymCommands = mutableMapOf<Pair<KeySym, Int>, String>()
+
+    private val modKeyBindings = mapOf(
+        Pair(Modifiers.CONTROL, setOf("Ctrl")),
+        Pair(Modifiers.SHIFT, setOf("Shift")),
+        Pair(Modifiers.ALT, setOf("Alt")),
+        Pair(Modifiers.SUPER, setOf("Super", "Win", "Lin")),
+        Pair(Modifiers.META, setOf("Meta")),
+        Pair(Modifiers.HYPER, setOf("Hyper"))
+    )
 
     init {
         for (keyConfig in configurationProvider.getProperyNames()) {
-            val keySym = inputApi.stringToKeysym(keyConfig)
-            keySymCommands[keySym] = configurationProvider[keyConfig]!!
+            val keyConfigParts = keyConfig.split('+')
+
+            val mask = if (keyConfigParts.size > 1) {
+                val modifier = modKeyBindings.entries
+                    .single { it.value.contains(keyConfigParts.first()) }
+                    .key
+                keyManager.modMasks.getOrElse(modifier) { 0 }
+            } else {
+                0
+            }
+
+            val keySym = inputApi.stringToKeysym(keyConfigParts.last())
+
+            keySymCommands[Pair(keySym, mask)] = configurationProvider[keyConfig]!!
         }
     }
 
-    fun getCommandForKey(keySym: KeySym, keyMask: Int): String? {
-        return keySymCommands[keySym]
+    fun getCommandForKey(keySym: KeySym, keyMask: UInt): String? {
+        return keySymCommands[Pair(keySym, filterMask(keyMask))]
+    }
+
+    private fun filterMask(keyMask: UInt): Int {
+        var filteredMask = keyMask.convert<Int>() and 0xFF
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.CAPS_LOCK, {0}).inv()
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.NUM_LOCK, {0}).inv()
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.SCROLL_LOCK, {0}).inv()
+        return filteredMask
     }
 }
