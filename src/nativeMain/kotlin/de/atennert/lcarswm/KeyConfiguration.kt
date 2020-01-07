d99836fd496251756ae39bf1ae5dffc16d954292
Andreas Tennert <mail@andreas-tennert.de>
2020-01-07 20:18:44 +0100

    refactor: extract key config interpretation functions

--- src/nativeMain/kotlin/de/atennert/lcarswm/KeyConfiguration.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/KeyConfiguration.kt
@@ -4,7 +4,11 @@ import de.atennert.lcarswm.system.api.InputApi
 import kotlinx.cinterop.convert
 import xlib.KeySym
 
-class KeyConfiguration(inputApi: InputApi, configurationProvider: Properties, private val keyManager: KeyManager) {
+class KeyConfiguration(
+    private val inputApi: InputApi,
+    configurationProvider: Properties,
+    private val keyManager: KeyManager
+) {
 
     private val keySymCommands = mutableMapOf<Pair<KeySym, Int>, String>()
 
@@ -19,21 +23,33 @@ class KeyConfiguration(inputApi: InputApi, configurationProvider: Properties, pr
 
     init {
         for (keyConfig in configurationProvider.getProperyNames()) {
-            val keyConfigParts = keyConfig.split('+')
+            val (modifierStrings, keyString) = separateKeySymAndModifiers(keyConfig)
 
-            val mask = if (keyConfigParts.size > 1) {
-                val modifier = modKeyBindings.entries
-                    .single { it.value.contains(keyConfigParts.first()) }
-                    .key
-                keyManager.modMasks.getOrElse(modifier) { 0 }
-            } else {
-                0
-            }
-
-            val keySym = inputApi.stringToKeysym(keyConfigParts.last())
+            val mask = getMask(modifierStrings)
+            val keySym = getKeySym(keyString)
 
             keySymCommands[Pair(keySym, mask)] = configurationProvider[keyConfig]!!
         }
+    }
+
+    private fun separateKeySymAndModifiers(keyConfig: String): Pair<List<String>, String> {
+        val keyStrings = keyConfig.split('+')
+        return Pair(keyStrings.dropLast(1), keyStrings.last())
+    }
+
+    private fun getMask(modifierStrings: List<String>): Int {
+        return if (modifierStrings.isNotEmpty()) {
+            val modifier = modKeyBindings.entries
+                .single { it.value.contains(modifierStrings.first()) }
+                .key
+            keyManager.modMasks.getOrElse(modifier) { 0 }
+        } else {
+            0
+        }
+    }
+
+    private fun getKeySym(keyString: String): KeySym {
+        return inputApi.stringToKeysym(keyString)
     }
 
     fun getCommandForKey(keySym: KeySym, keyMask: UInt): String? {
@@ -42,9 +58,9 @@ class KeyConfiguration(inputApi: InputApi, configurationProvider: Properties, pr
 
     private fun filterMask(keyMask: UInt): Int {
         var filteredMask = keyMask.convert<Int>() and 0xFF
-        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.CAPS_LOCK, {0}).inv()
-        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.NUM_LOCK, {0}).inv()
-        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.SCROLL_LOCK, {0}).inv()
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.CAPS_LOCK, { 0 }).inv()
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.NUM_LOCK, { 0 }).inv()
+        filteredMask = filteredMask and keyManager.modMasks.getOrElse(Modifiers.SCROLL_LOCK, { 0 }).inv()
         return filteredMask
     }
 }
