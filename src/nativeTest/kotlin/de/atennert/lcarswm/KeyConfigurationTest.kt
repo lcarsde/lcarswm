d5424b63ec5e4ed93e118837ebd753102837a954
Andreas Tennert <mail@andreas-tennert.de>
2020-01-07 20:08:36 +0100

    test: load key config which has properties with up to one modifier

--- src/nativeTest/kotlin/de/atennert/lcarswm/KeyConfigurationTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/KeyConfigurationTest.kt
@@ -14,9 +14,11 @@ class KeyConfigurationTest {
     @Test
     fun `load simple key configuration`() {
         val systemApi = SystemFacadeMock()
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+
         val configurationProvider = object : Properties {
             override fun get(propertyKey: String): String? {
-                return when(propertyKey) {
+                return when (propertyKey) {
                     "A" -> "commandA"
                     "B" -> "commandB"
                     "X" -> "commandX"
@@ -28,12 +30,69 @@ class KeyConfigurationTest {
                 return setOf("A", "B", "X")
             }
         }
-        val keyConfiguration = KeyConfiguration(systemApi, configurationProvider)
+        val keyConfiguration = KeyConfiguration(systemApi, configurationProvider, keyManager)
 
-        assertEquals("commandA", keyConfiguration.getCommandForKey(XK_A.convert(), 0), "The config should load the first key binding")
-        assertEquals("commandB", keyConfiguration.getCommandForKey(XK_B.convert(), 0), "The config should load the second key binding")
-        assertEquals("commandX", keyConfiguration.getCommandForKey(XK_X.convert(), 0), "The config should load the third key binding")
+        assertEquals(
+            "commandA",
+            keyConfiguration.getCommandForKey(XK_A.convert(), 0.convert()),
+            "The config should load the first key binding"
+        )
+        assertEquals(
+            "commandB",
+            keyConfiguration.getCommandForKey(XK_B.convert(), 0.convert()),
+            "The config should load the second key binding"
+        )
+        assertEquals(
+            "commandX",
+            keyConfiguration.getCommandForKey(XK_X.convert(), 0.convert()),
+            "The config should load the third key binding"
+        )
 
-        assertNull(keyConfiguration.getCommandForKey(XK_C.convert(), 0), "The config should not provide an unknown key binding")
+        assertNull(
+            keyConfiguration.getCommandForKey(XK_C.convert(), 0.convert()),
+            "The config should not provide an unknown key binding"
+        )
     }
+
+    @Test
+    fun `load key config including one modifier`() {
+        val systemApi = SystemFacadeMock()
+        val keyManager = KeyManager(systemApi, systemApi.rootWindowId)
+
+        val configurationProvider = object : Properties {
+            override fun get(propertyKey: String): String? {
+                return when (propertyKey) {
+                    "A" -> "commandA"
+                    "Ctrl+B" -> "commandB"
+                    "Alt+X" -> "commandX"
+                    else -> error("unknown key configuration: $propertyKey")
+                }
+            }
+
+            override fun getProperyNames(): Set<String> {
+                return setOf("A", "Ctrl+B", "Alt+X")
+            }
+        }
+        val keyConfiguration = KeyConfiguration(systemApi, configurationProvider, keyManager)
+
+        assertEquals(
+            "commandA",
+            keyConfiguration.getCommandForKey(XK_A.convert(), 0.convert()),
+            "The config should load the first key binding"
+        )
+        assertEquals(
+            "commandB",
+            keyConfiguration.getCommandForKey(XK_B.convert(), keyManager.modMasks.getValue(Modifiers.CONTROL).convert()),
+            "The config should load the second key binding"
+        )
+        assertEquals(
+            "commandX",
+            keyConfiguration.getCommandForKey(XK_X.convert(), keyManager.modMasks.getValue(Modifiers.ALT).convert()),
+            "The config should load the third key binding"
+        )
+    }
+
+    // TODO two modifier key config
+
+    // TODO three modifier key config
 }
