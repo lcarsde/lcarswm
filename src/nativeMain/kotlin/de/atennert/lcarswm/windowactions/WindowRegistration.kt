637892b1d0ee171ca3c19db466d46bd797c7d9ad
Andreas Tennert <mail@andreas-tennert.de>
2020-01-24 19:26:39 +0100

    doc: complete the comments for the window registration interface

--- src/nativeMain/kotlin/de/atennert/lcarswm/windowactions/WindowRegistration.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/windowactions/WindowRegistration.kt
@@ -6,9 +6,21 @@ import xlib.Window
  * Interface to the window registration, which handles windows, not monitors and not their combination.
  */
 interface WindowRegistration {
+    /**
+     * Register a new window to the window manager.
+     * @param windowId ID of the window to register
+     * @param isSetup true if the adding is happening in setup stage, false otherwise
+     */
     fun addWindow(windowId: Window, isSetup: Boolean)
 
+    /**
+     * @return true, if the window with the given window ID is managed by the window manager, false otherwise
+     */
     fun isWindowManaged(windowId: Window): Boolean
 
+    /**
+     * Unregister a window from the window manager.
+     * @param windowId ID of the window to unregister
+     */
     fun removeWindow(windowId: Window)
 }
