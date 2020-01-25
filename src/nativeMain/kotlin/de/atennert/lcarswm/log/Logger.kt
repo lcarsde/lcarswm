deefb157f81f4fab98dfc509d2d30a4b24ea6384
Andreas Tennert <mail@andreas-tennert.de>
2020-01-25 21:51:07 +0100

    doc: add comments for the logger interface

--- src/nativeMain/kotlin/de/atennert/lcarswm/log/Logger.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/log/Logger.kt
@@ -1,13 +1,21 @@
 package de.atennert.lcarswm.log
 
+/**
+ * Interface for logger implementations
+ */
 interface Logger {
+    /** log information in debug mode */
     fun logDebug(text: String)
 
+    /** log information in info mode */
     fun logInfo(text: String)
 
+    /** log information in warning mode */
     fun logWarning(text: String)
 
+    /** log information in error mode */
     fun logError(text: String)
 
+    /** close logger and free any used resources */
     fun close()
 }
