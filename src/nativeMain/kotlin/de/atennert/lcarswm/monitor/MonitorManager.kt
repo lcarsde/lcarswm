d148c6b3f8dc3d351dafa3639f328b39c860af9f
Andreas Tennert <mail@andreas-tennert.de>
2020-01-27 21:26:03 +0100

    doc: add comments to monitor manager interface

--- src/nativeMain/kotlin/de/atennert/lcarswm/monitor/MonitorManager.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/monitor/MonitorManager.kt
@@ -2,16 +2,38 @@ package de.atennert.lcarswm.monitor
 
 import de.atennert.lcarswm.ScreenMode
 
+/**
+ * Interface for manager for handling monitors and the current screen mode for those monitors.
+ * The screen modes are defined in {@link ScreenMode}
+ */
 interface MonitorManager {
+    /**
+     * Update the list of known active monitors
+     */
     fun updateMonitorList()
 
+    /**
+     * @return the list of known active monitors
+     */
     fun getMonitors(): List<Monitor>
 
+    /**
+     * @return the primary monitor, if there's no monitor marked as primary monitor, the monitor manager will select the first monitor it finds as primary
+     */
     fun getPrimaryMonitor(): Monitor
 
+    /**
+     * @return the combined size of all monitors, depending on the configured monitor screen size and the positioning of the monitors
+     */
     fun getCombinedScreenSize(): Pair<Int, Int>
 
+    /**
+     * @return the current screen mode
+     */
     fun getScreenMode(): ScreenMode
 
+    /**
+     * Toggle between all ScreenModes
+     */
     fun toggleScreenMode()
 }
