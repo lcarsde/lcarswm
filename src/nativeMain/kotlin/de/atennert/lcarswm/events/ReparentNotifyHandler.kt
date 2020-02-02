0ee4f822a420d401c3befe5a856cd0e1be539521
Andreas Tennert <mail@andreas-tennert.de>
2020-02-02 19:21:32 +0100

    feat: check for parent window on reparent notify

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/ReparentNotifyHandler.kt
@@ -0,0 +1,25 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.log.Logger
+import de.atennert.lcarswm.windowactions.WindowRegistration
+import xlib.ReparentNotify
+import xlib.XEvent
+
+class ReparentNotifyHandler(
+    private val logger: Logger,
+    private val windowRegistration: WindowRegistration
+) : XEventHandler {
+    override val xEventType = ReparentNotify
+
+    override fun handleEvent(event: XEvent): Boolean {
+        val windowId = event.xreparent.window
+        val parentId = event.xreparent.parent
+        logger.logDebug("ReparentNotifyHandler::handleEvent::reparented $windowId to $parentId")
+
+        // bad reparenting ... remove the window from the registration
+        if (!windowRegistration.isWindowParentedBy(windowId, parentId)) {
+            windowRegistration.removeWindow(windowId)
+        }
+        return false
+    }
+}
---
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/ReparentNotifyHandlerTest.kt
@@ -0,0 +1,45 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.log.LoggerMock
+import de.atennert.lcarswm.system.SystemFacadeMock
+import de.atennert.lcarswm.windowactions.WindowRegistrationMock
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.nativeHeap
+import xlib.ReparentNotify
+import xlib.Window
+import xlib.XEvent
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+
+class ReparentNotifyHandlerTest {
+    @Test
+    fun `handler should have correct type`() {
+        assertEquals(ReparentNotify, ReparentNotifyHandler(LoggerMock(), WindowRegistrationMock()).xEventType, "The type should be ReparentNotify")
+    }
+
+    @Test
+    fun `unmanage a client that is not reparented to its desired frame`() {
+        val system = SystemFacadeMock()
+        val windowId = system.getNewWindowId()
+        val parentId = system.getNewWindowId()
+        val windowHandler = object : WindowRegistrationMock() {
+            override fun isWindowParentedBy(windowId: Window, parentId: Window): Boolean = false
+        }
+
+        val reparentEvent = nativeHeap.alloc<XEvent>()
+        reparentEvent.type = ReparentNotify
+        reparentEvent.xreparent.window = windowId
+        reparentEvent.xreparent.parent = parentId
+
+        val reparentNotifyHandler = ReparentNotifyHandler(LoggerMock(), windowHandler)
+
+        val shutdownValue = reparentNotifyHandler.handleEvent(reparentEvent)
+
+        assertFalse(shutdownValue, "ReparentNotify should not trigger a shutdown")
+
+        val removeWindowCall = windowHandler.functionCalls.removeAt(0)
+        assertEquals("removeWindow", removeWindowCall.name, "Remove a window with bad reparenting")
+        assertEquals(windowId, removeWindowCall.parameters[0])
+    }
+}
