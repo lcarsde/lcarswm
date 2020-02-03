c024e8773b8946bc4bc0e3009e5957bef99ba8f0
Andreas Tennert <mail@andreas-tennert.de>
2020-02-03 18:40:23 +0100

    feat: create ClientMessage handler for logging

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/ClientMessageHandler.kt
@@ -0,0 +1,21 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.atom.AtomLibrary
+import de.atennert.lcarswm.atom.Atoms
+import de.atennert.lcarswm.log.Logger
+import xlib.ClientMessage
+import xlib.XEvent
+
+class ClientMessageHandler(private val logger: Logger, private val atomLibrary: AtomLibrary) : XEventHandler {
+    override val xEventType = ClientMessage
+
+    override fun handleEvent(event: XEvent): Boolean {
+        val window = event.xclient.window
+        val messageType = event.xclient.message_type
+        val atom = Atoms.values().find { atomLibrary[it] == messageType }
+
+        logger.logDebug("ClientMessageHandler::handleEvent::window: $window, message type: $messageType, atom: $atom")
+
+        return false
+    }
+}
---
+++ src/nativeTest/kotlin/de/atennert/lcarswm/events/ClientMessageHandlerTest.kt
@@ -0,0 +1,23 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.atom.AtomLibrary
+import de.atennert.lcarswm.log.LoggerMock
+import de.atennert.lcarswm.system.SystemFacadeMock
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.nativeHeap
+import xlib.ClientMessage
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+
+class ClientMessageHandlerTest {
+    @Test
+    fun `check correct type`() {
+        assertEquals(ClientMessage, ClientMessageHandler(LoggerMock(), AtomLibrary(SystemFacadeMock())).xEventType, "The message handler should have the correct type")
+    }
+
+    @Test
+    fun `check not shutting down`() {
+        assertFalse(ClientMessageHandler(LoggerMock(), AtomLibrary(SystemFacadeMock())).handleEvent(nativeHeap.alloc()), "The message handler should not trigger a shutdown of the WM")
+    }
+}
