2fcbe8ea99029c65fec4292d5a168902b53b79b0
Andreas Tennert <mail@andreas-tennert.de>
2020-02-08 20:02:52 +0100

    doc: complete comments in atom library

--- src/nativeMain/kotlin/de/atennert/lcarswm/atom/AtomLibrary.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/atom/AtomLibrary.kt
@@ -8,7 +8,13 @@ import xlib.Atom
  */
 class AtomLibrary(windowUtils: WindowUtilApi) {
 
+    /**
+     * Map of atoms that are used by the window manager. The atoms are initialized at start.
+     */
     private val atomMap: Map<Atoms, Atom> = Atoms.values().asList().associateWith { windowUtils.internAtom(it.atomName) }
 
+    /**
+     * Get the atom value for an atom enum value
+     */
     operator fun get(atom: Atoms): Atom = atomMap.getValue(atom)
 }
