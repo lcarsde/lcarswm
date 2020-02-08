ce43482638b5f3d8c0e7271bbad476375677de52
Andreas Tennert <mail@andreas-tennert.de>
2020-02-08 20:07:25 +0100

    doc: complete comments for ubyte array conversions

--- src/nativeMain/kotlin/de/atennert/lcarswm/conversion/ubyteArrayConversions.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/conversion/ubyteArrayConversions.kt
@@ -2,6 +2,7 @@ package de.atennert.lcarswm.conversion
 
 import kotlinx.cinterop.convert
 
+/** Convert ULong value to byte array as used in X properties */
 fun ULong.toUByteArray(): UByteArray {
     return ubyteArrayOf(
         (this and 0xFF.convert()).convert(),
@@ -11,10 +12,12 @@ fun ULong.toUByteArray(): UByteArray {
     )
 }
 
+/** Convert string to byte array as used in X properties */
 fun String.toUByteArray(): UByteArray {
     return this.encodeToByteArray().asUByteArray()
 }
 
+/** Concatenate UByte arrays to a single UByte array */
 fun List<UByteArray>.combine(): UByteArray {
     val byteList = this.fold(mutableListOf<UByte>()) { list, bytes -> list.addAll(bytes); list }
     return byteList.toUByteArray()
