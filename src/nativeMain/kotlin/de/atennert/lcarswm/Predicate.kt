ee44a2bfe3f11e7d41f26ea7d391273ed4af1f1e
Andreas Tennert <mail@andreas-tennert.de>
2020-02-13 20:30:44 +0100

    doc: document the predicate definition

--- src/nativeMain/kotlin/de/atennert/lcarswm/Predicate.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/Predicate.kt
@@ -1,3 +1,9 @@
 package de.atennert.lcarswm
 
+/**
+ * Definition of predicates for checking elements.
+ *
+ * @param T type of elements that are checked with the predicate
+ * @return true if the element matches the requirements, false otherwise
+ */
 typealias Predicate<T> = (x: T) -> Boolean
