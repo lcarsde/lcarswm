97440a0d11e4783060a7b78f97bb6bf83e4c0aaf
Andreas Tennert <mail@andreas-tennert.de>
2020-02-14 21:16:47 +0100

    feat: implement an event buffer

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/EventBuffer.kt
@@ -0,0 +1,57 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.Predicate
+import de.atennert.lcarswm.system.api.EventApi
+import kotlinx.cinterop.*
+import xlib.QueuedAfterFlush
+import xlib.Success
+import xlib.XEvent
+
+class EventBuffer(private val eventApi: EventApi) {
+    private val internalEventQueue = mutableListOf<CPointer<XEvent>>()
+
+    fun findEvent(blocking: Boolean, predicate: Predicate<CPointer<XEvent>>): CPointer<XEvent>? {
+        while (true) {
+            val eventMatch = internalEventQueue.find(predicate)
+            if (eventMatch != null) {
+                return eventMatch
+            }
+            if (!readEvents(blocking)) {
+                break
+            }
+        }
+        return null
+    }
+
+    fun getNextEvent(blocking: Boolean): CPointer<XEvent>? {
+        if (internalEventQueue.isEmpty()) {
+            readEvents(blocking)
+        }
+        return if (internalEventQueue.isNotEmpty()) {
+            internalEventQueue.removeAt(0)
+        } else {
+            null
+        }
+    }
+
+    private fun readEvents(blocking: Boolean): Boolean {
+        var n = eventApi.getQueuedEvents(QueuedAfterFlush)
+        var alreadyRun = false
+
+        while ((blocking and !alreadyRun) or (n > 0)) {
+            val event = nativeHeap.alloc<XEvent>()
+
+            if (eventApi.nextEvent(event.ptr) != Success) {
+                nativeHeap.free(event)
+                return false
+            }
+
+            internalEventQueue.add(event.ptr)
+
+            --n
+            alreadyRun = true
+        }
+
+        return alreadyRun
+    }
+}
