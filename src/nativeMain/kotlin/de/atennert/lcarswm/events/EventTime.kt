efac64182f32a469c43abdc2bdfff3c64238ee4b
Andreas Tennert <mail@andreas-tennert.de>
2020-02-17 20:14:43 +0100

    feat: add event time tracker

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/events/EventTime.kt
@@ -0,0 +1,66 @@
+package de.atennert.lcarswm.events
+
+import de.atennert.lcarswm.RootWindowPropertyHandler
+import de.atennert.lcarswm.atom.AtomLibrary
+import de.atennert.lcarswm.atom.Atoms
+import de.atennert.lcarswm.system.api.WindowUtilApi
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.pointed
+import xlib.*
+
+class EventTime(
+    private val windowUtilApi: WindowUtilApi,
+    private val eventBuffer: EventBuffer,
+    private val atomLibrary: AtomLibrary,
+    private val rootWindowPropertyHandler: RootWindowPropertyHandler
+) {
+    private var _lastEventTime: Time = CurrentTime.convert()
+    val lastEventTime: Time
+        get() {
+            if (_lastEventTime == CurrentTime.convert<Time>()) {
+                triggerTimeUpdate()
+            }
+            return _lastEventTime
+        }
+
+    private fun triggerTimeUpdate() {
+        windowUtilApi.changeProperty(
+            rootWindowPropertyHandler.ewmhSupportWindow,
+            atomLibrary[Atoms.WM_CLASS],
+            atomLibrary[Atoms.STRING],
+            null,
+            8,
+            PropModeAppend
+        )
+        eventBuffer.findEvent(true) { this.findEvent(it) }
+    }
+
+    private fun findEvent(event: CPointer<XEvent>): Boolean {
+        val newTime = getTimeFromEvent(event)
+        return if (newTime != CurrentTime.convert<Time>() && newTime != _lastEventTime) {
+            _lastEventTime = newTime
+            true
+        } else {
+            false
+        }
+    }
+
+    fun setTimeFromEvent(event: CPointer<XEvent>) {
+        _lastEventTime = getTimeFromEvent(event)
+    }
+
+    private fun getTimeFromEvent(event: CPointer<XEvent>): Time {
+        return when (event.pointed.type) {
+            ButtonPress -> event.pointed.xbutton.time
+            ButtonRelease -> event.pointed.xbutton.time
+            KeyPress -> event.pointed.xkey.time
+            KeyRelease -> event.pointed.xkey.time
+            MotionNotify -> event.pointed.xmotion.time
+            PropertyNotify -> event.pointed.xproperty.time
+            EnterNotify -> event.pointed.xcrossing.time
+            LeaveNotify -> event.pointed.xcrossing.time
+            else -> CurrentTime.convert()
+        }
+    }
+}
