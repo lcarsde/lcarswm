5ca4811d56535ffbd64342e29229f2a9da6477b1
Andreas Tennert <mail@andreas-tennert.de>
2020-03-01 18:21:21 +0100

    feat: initialize core signal handling

--- src/nativeMain/kotlin/de/atennert/lcarswm/signal/SignalHandler.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/signal/SignalHandler.kt
@@ -10,17 +10,24 @@ class SignalHandler(posixApi: PosixApi) {
 
     private val allSignals: sigset_t = nativeHeap.alloc()
 
+    private val oldActions = mutableMapOf<Int, sigaction>()
+
     init {
         val action = nativeHeap.alloc<sigaction>()
 
         posixApi.sigFillSet(allSignals.ptr)
         posixApi.sigEmptySet(action.sa_mask.ptr)
 
-        action.__sigaction_handler.sa_handler = staticCFunction { signal: Int -> handleSignal(signal) }
+        action.__sigaction_handler.sa_handler = staticCFunction { signal -> handleSignal(signal) }
         action.sa_flags = SA_NOCLDSTOP
 
-        Signal.CORE_DUMP_SIGNALS.forEach {
-            // TODO register actions
+        Signal.CORE_DUMP_SIGNALS
+                // don't register for ABRT because it will be triggered by abort from other core signals
+            .filter { it != Signal.ABRT }
+            .forEach {
+                val oldSignal = nativeHeap.alloc<sigaction>()
+                posixApi.sigAction(it, action.ptr, oldSignal.ptr)
+                oldActions[it.signalValue] = oldSignal
         }
     }
 
