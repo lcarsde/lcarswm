9e327992b16ae4cb9d54e4e282dd4a5585292438
Andreas Tennert <mail@andreas-tennert.de>
2020-03-10 20:16:52 +0100

    test: add external signal handlers

--- src/nativeTest/kotlin/de/atennert/lcarswm/signal/SignalHandlerTest.kt
+++ src/nativeTest/kotlin/de/atennert/lcarswm/signal/SignalHandlerTest.kt
@@ -3,10 +3,17 @@ package de.atennert.lcarswm.signal
 import de.atennert.lcarswm.system.SystemFacadeMock
 import kotlinx.cinterop.invoke
 import kotlinx.cinterop.pointed
+import kotlinx.cinterop.staticCFunction
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SignalHandlerTest {
+    @BeforeTest
+    fun setup() {
+        receivedSignal = null
+    }
+
     @Test
     fun `handle core signal`() {
         val system = SystemFacadeMock()
@@ -21,4 +28,20 @@ class SignalHandlerTest {
                 assertEquals("abort", abortCall.name, "Call abort for signal $signal")
             }
     }
+
+    @Test
+    fun `add signal callback`() {
+        val system = SystemFacadeMock()
+        val signalHandler = SignalHandler(system)
+
+        system.functionCalls.clear()
+
+        signalHandler.addSignalCallback(Signal.INT, staticCFunction<Int, Unit> { receivedSignal = it })
+
+        system.signalActions[Signal.INT]?.pointed?.__sigaction_handler?.sa_handler?.invoke(Signal.INT.signalValue)
+
+        assertEquals(Signal.INT.signalValue, receivedSignal, "We should get the signal, that we registered for")
+    }
 }
+
+private var receivedSignal: Int? = null
