5013ef2996525ab8bce103bbc3814f2ed5f2a523
Andreas Tennert <mail@andreas-tennert.de>
2020-03-31 20:16:24 +0200

    feat: add FontApi to SystemApi

--- src/nativeMain/kotlin/de/atennert/lcarswm/system/api/SystemApi.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/system/api/SystemApi.kt
@@ -3,4 +3,4 @@ package de.atennert.lcarswm.system.api
 /**
  * API that combines all other APIs for system access
  */
-interface SystemApi: RandrApi, DrawApi, InputApi, EventApi, WindowUtilApi, PosixApi
+interface SystemApi: RandrApi, DrawApi, InputApi, EventApi, WindowUtilApi, PosixApi, FontApi
