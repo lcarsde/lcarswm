805debd8b3432b86d6b387812df7de7ba50ca751
Andreas Tennert <mail@andreas-tennert.de>
2020-04-01 21:47:53 +0200

    feat: add newLayout to font api

--- src/nativeMain/kotlin/de/atennert/lcarswm/system/api/FontApi.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/system/api/FontApi.kt
@@ -2,7 +2,10 @@ package de.atennert.lcarswm.system.api
 
 import kotlinx.cinterop.CPointer
 import xlib.PangoContext
+import xlib.PangoLayout
 
 interface FontApi {
     fun xftGetContext(screen: Int): CPointer<PangoContext>?
+
+    fun newLayout(pango: CPointer<PangoContext>?): CPointer<PangoLayout>?
 }
