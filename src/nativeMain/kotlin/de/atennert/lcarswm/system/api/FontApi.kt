21d3c09e494243dd70d2de0caabe8a53051012a2
Andreas Tennert <mail@andreas-tennert.de>
2020-04-02 20:30:41 +0200

    feat: add get font description to font API

--- src/nativeMain/kotlin/de/atennert/lcarswm/system/api/FontApi.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/system/api/FontApi.kt
@@ -2,10 +2,13 @@ package de.atennert.lcarswm.system.api
 
 import kotlinx.cinterop.CPointer
 import xlib.PangoContext
+import xlib.PangoFontDescription
 import xlib.PangoLayout
 
 interface FontApi {
     fun xftGetContext(screen: Int): CPointer<PangoContext>?
 
     fun newLayout(pango: CPointer<PangoContext>?): CPointer<PangoLayout>?
+
+    fun getFontDescription(): CPointer<PangoFontDescription>?
 }
