c3f319a5e56f6a8e9404eff1ae2b01b780806136
Andreas Tennert <mail@andreas-tennert.de>
2020-04-19 13:11:30 +0200

    ci: use deployment always for testing

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/windowactions/WindowList.kt
@@ -0,0 +1,32 @@
+package de.atennert.lcarswm.windowactions
+
+import de.atennert.lcarswm.FramedWindow
+import xlib.Window
+
+class WindowList {
+    private val windows = mutableSetOf<FramedWindow>()
+
+    fun add(window: FramedWindow) {
+        windows.add(window)
+    }
+
+    fun remove(window: FramedWindow) {
+        windows.remove(window)
+    }
+
+    fun remove(windowId: Window) {
+        windows.removeAll { it.id == windowId }
+    }
+
+    fun get(windowId: Window): FramedWindow? {
+        return windows.firstOrNull { it.id == windowId }
+    }
+
+    fun update(window: FramedWindow) {
+        windows.removeAll { it.id == window.id }
+    }
+
+    fun isManaged(windowId: Window): Boolean {
+        return windows.any { it.id == windowId }
+    }
+}
---
+++ src/nativeTest/kotlin/WindowFocusTest.kt
@@ -0,0 +1,28 @@
+import de.atennert.lcarswm.system.FunctionCall
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.pointed
+import xlib.Above
+import xlib.CWStackMode
+import xlib.Window
+import xlib.XWindowChanges
+import kotlin.test.assertEquals
+
+class WindowFocusTest {
+    // TODO check adding
+
+    // TODO check toggling
+
+    // TODO check removing
+
+    // TODO toggle with one window
+
+    // TODO toggle with no window
+
+    private fun checkRestacking(restackCall: FunctionCall, window: Window) {
+        assertEquals("configureWindow", restackCall.name, "The window $window needs to be configured")
+        assertEquals(window, restackCall.parameters[0], "The _window ${window}_ needs to be configured")
+        assertEquals(CWStackMode.convert<UInt>(), restackCall.parameters[1], "The $window window needs to be restacked")
+        assertEquals(Above, (restackCall.parameters[2] as CPointer<XWindowChanges>).pointed.stack_mode, "The stack mode should be 'above'")
+    }
+}
