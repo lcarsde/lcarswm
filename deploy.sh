ee1a96073dc911b377841c8c55aeee93f5e5cf6c
Andreas Tennert <mail@andreas-tennert.de>
2020-04-19 16:28:48 +0200

    ci: create tarball for debian package

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,6 @@ echo "build debian package for lcarswm-${TRAVIS_TAG}"
 # create debian package construction directory
 mkdir -p "build/deb/lcarswm-${TRAVIS_TAG}"
 
-# copy debian packaging files
-cp -r "debian" "build/deb/lcarswm-${TRAVIS_TAG}/debian"
-
 # create software directories
 mkdir -p "build/deb/lcarswm-${TRAVIS_TAG}/usr/bin"
 
@@ -20,6 +17,17 @@ mkdir -p "build/deb/lcarswm-${TRAVIS_TAG}/usr/bin"
 cp "build/bin/native/releaseExecutable/lcarswm.kexe" "build/deb/lcarswm-${TRAVIS_TAG}/usr/bin/"
 cp -r "build/processedResources/native/main/usr/share" "build/deb/lcarswm-${TRAVIS_TAG}/usr/share"
 
+cd "build/deb"
+
+echo "create tarball"
+tar -czvf "lcarswm_${TRAVIS_TAG}.tar.gz" "lcarswm-${TRAVIS_TAG}"
+
+tar -ztvf "lcarswm_${TRAVIS_TAG}.tar.gz"
+
+cd ../..
+
+# copy debian packaging files
+cp -r "debian" "build/deb/lcarswm-${TRAVIS_TAG}/debian"
 
 echo "Deploying debian package of lcarswm-${TRAVIS_TAG}"
 
