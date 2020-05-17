9b9306af6671d006f0469af376017f6600bc89a8
Andreas Tennert <mail@andreas-tennert.de>
2020-05-17 20:49:37 +0200

    feat: show app menu buttons from windows

--- app-selector/lcarswm-app-selector.py
+++ app-selector/lcarswm-app-selector.py
@@ -3,20 +3,19 @@ from threading import Thread
 from posix_ipc import MessageQueue, BusyError
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import GdkX11, Gtk, GObject
+from gi.repository import GdkX11, Gdk, Gtk, GLib
 
 
 class LcarswmAppSelector(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello World")
 
-        self.button = Gtk.Button(label="Click Me")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
-        self.set_decorated(False)
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        self.add(self.box)
+        self.buttons = {}
 
-        GObject.signal_new("list-update-signal", self, GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (GObject.TYPE_STRING,))
-        self.connect("list-update-signal", self.on_list_update)
+        self.set_decorated(False)
+        self.override_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(0, 0, 0, 1))
 
         self.stop_threads = False
         self.thread = Thread(target=self.read_window_list_from_queue, args=(lambda: self.stop_threads, self))
@@ -26,8 +25,8 @@ class LcarswmAppSelector(Gtk.Window):
         self.connect("destroy", self.on_destroy)
 
     def on_create(self, window):
+        # mark myself as the app menu
         self.get_property("window").set_utf8_property("LCARSWM_APP_SELECTOR", "LCARSWM_APP_SELECTOR")
-        print("realized", self.get_property("window").get_xid())
         self.thread.start()
 
     def on_destroy(self, window):
@@ -40,7 +39,7 @@ class LcarswmAppSelector(Gtk.Window):
         while True:
             try:
                 s, _ = mq.receive(.4)
-                window.emit("list-update-signal", s.decode('utf-8'))
+                GLib.idle_add(window.on_list_update, window, s.decode('utf-8'))
             except BusyError:
                 pass
 
@@ -51,7 +50,29 @@ class LcarswmAppSelector(Gtk.Window):
 
     @staticmethod
     def on_list_update(self, list_string):
-        print("Received:", list_string)
+        updated_window_elements = dict((window_id, class_name) for window_id, class_name in
+                                       (window_element.split("\t") for window_element in list_string.split("\n")))
+
+        known_windows = list(self.buttons.keys())
+        # cleanup entries that don't exist anymore
+        for known_window_id in known_windows:
+            if known_window_id not in updated_window_elements.keys():
+                button = self.buttons[known_window_id]
+                self.box.remove(button)
+                del self.buttons[known_window_id]
+
+        for (window_id, class_name) in updated_window_elements.items():
+            if window_id in known_windows:
+                # update entries
+                button = self.buttons[window_id]
+                button.set_label(class_name)
+            else:
+                # add new entries
+                button = Gtk.Button(label=class_name)
+                button.connect("clicked", self.on_button_clicked)
+                self.box.pack_start(button, False, False, 0)
+                self.buttons[window_id] = button
+        self.show_all()
 
     @staticmethod
     def on_button_clicked(widget):
