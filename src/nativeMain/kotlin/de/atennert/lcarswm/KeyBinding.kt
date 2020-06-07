fc625e436af1bc9b8f731cef9ddd77975e08956d
Andreas Tennert <mail@andreas-tennert.de>
2020-06-07 20:19:12 +0200

    feat: create key binding and sub classes key action and key execution

---
+++ src/nativeMain/kotlin/de/atennert/lcarswm/KeyBinding.kt
@@ -0,0 +1,12 @@
+package de.atennert.lcarswm
+
+sealed class KeyBinding {
+    abstract val keys: String
+    abstract val command: String
+}
+
+/** The key execution defines a key binding for executing a command line command with exec */
+data class KeyExecution(override val keys: String, override val command: String) : KeyBinding()
+
+/** The key action defines a key binding for a window manager action */
+data class KeyAction(override val keys: String, override val command: String) : KeyBinding()
