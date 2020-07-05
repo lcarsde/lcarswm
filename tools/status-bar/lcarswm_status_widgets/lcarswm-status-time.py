aedda48f4d48e40381130c3de57d18bba74f10b0
Andreas Tennert <mail@andreas-tennert.de>
2020-07-05 21:06:50 +0200

    feat: starting initial work on time widget and defining package

---
+++ tools/status-bar/lcarswm_status_widgets/lcarswm-status-time.py
@@ -0,0 +1,5 @@
+from lcarswm_status_widgets import LcarswmStatusWidget
+
+class LcarswmStatusTime(LcarswmStatusWidget):
+    def __init__(self, width, height, css_provider):
+        super().__init__(width, height, css_provider)
