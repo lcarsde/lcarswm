d586457930a1a6dbf80b3e4c79d78460d98157fa
Andreas Tennert <mail@andreas-tennert.de>
2020-07-06 19:50:00 +0200

    feat: add comments and update function in abstract widget class

--- tools/status-bar/lcarswm_status_widgets/lcarswm-status-widget.pyi
+++ tools/status-bar/lcarswm_status_widgets/lcarswm-status-widget.pyi
@@ -4,9 +4,18 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import GdkX11, Gdk, Gtk, GLib
 
 class LcarswmStatusWidget(Gtk.Widget):
+    """
+    General widget frame for lcarswm status widgets
+    """
     def __init__(self, width, height, css_provider):
         self.width = width
         self.heigth = height
 
         self.get_style_context().add_class("status_bar_widget")
         self.get_style_context().add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
+    def update(self):
+        """
+        Called for triggering an update of the widget data
+        """
+        pass
