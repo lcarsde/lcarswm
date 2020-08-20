b4016f4113da14426abfe0816e9680ed5dfcddf3
Andreas Tennert <mail@andreas-tennert.de>
2020-08-20 08:29:17 +0200

    build: upgrade Kotlin version to 1.4.0

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("multiplatform") version "1.4.0"
+}
+
+group "de.atennert"
+version "20.2"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    linuxX64("native") {
+        binaries {
+            executable()
+        }
+        compilations.getByName("main") {
+            val xlib by cinterops.creating
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+            }
+        }
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "6.5.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
