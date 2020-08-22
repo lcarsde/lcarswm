a3bccb440f306c89bc77f6668da7696d2942ffa7
Andreas Tennert <mail@andreas-tennert.de>
2020-08-22 16:55:21 +0200

    feat: display the status bar widgets from the right side

--- tools/status-bar/lcarswm_status_bar.py
+++ tools/status-bar/lcarswm_status_bar.py
@@ -2,6 +2,8 @@
 from threading import Thread
 from time import sleep
 import importlib
+import numpy as np
+from lcarswm import status_widget
 
 import gi
 gi.require_version("Gtk", "3.0")
@@ -82,16 +84,16 @@ class LcarswmStatusBar(Gtk.Window):
         self.get_style_context().add_class("window")
         self.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
 
-        self.width = 640
+        self.width = 40
         self.height = LcarswmStatusBar.get_pixels_for_cells(3)  # remains constant by design
 
-        grid = Gtk.Grid()
-        grid.set_column_spacing(GAP_SIZE)
-        grid.set_row_spacing(GAP_SIZE)
-        grid.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
-        self.add(grid)
+        self.grid = Gtk.Grid()
+        self.grid.set_column_spacing(GAP_SIZE)
+        self.grid.set_row_spacing(GAP_SIZE)
+        self.grid.get_style_context().add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+        self.add(self.grid)
 
-        self.widget_dict = self.import_widgets(grid)
+        self.widget_dict = self.import_widgets()
 
         self.stop_threads = False
         self.update_thread = Thread(target=self.update_widgets, args=(lambda: self.stop_threads, self))
@@ -116,7 +118,7 @@ class LcarswmStatusBar(Gtk.Window):
             self.width = new_width
             self.update_layout()
 
-    def import_widgets(self, grid):
+    def import_widgets(self):
         widget_dict = {}
         for widget_config in get_widgets():
             widget = getattr(importlib.import_module(widget_config.module, widget_config.package), widget_config.widget)
@@ -124,8 +126,6 @@ class LcarswmStatusBar(Gtk.Window):
                                      LcarswmStatusBar.get_pixels_for_cells(widget_config.height),
                                      self.css_provider)
             widget_dict[widget_instance] = widget_config
-            # TODO remove this
-            grid.attach(widget_instance, widget_config.x, widget_config.y, widget_config.width, widget_config.height)
         return widget_dict
 
     @staticmethod
@@ -133,16 +133,40 @@ class LcarswmStatusBar(Gtk.Window):
         return CELL_SIZE * cells + GAP_SIZE * (cells - 1)
 
     def update_layout(self):
-        # TODO update layout
-        horizontal_cells = int(self.width / CELL_SIZE)
-        left_over_pixels = self.width % CELL_SIZE
-        print(self.width, horizontal_cells, left_over_pixels)
+        horizontal_cells = int(self.width / (CELL_SIZE + GAP_SIZE))
+        left_over_pixels = self.width % (CELL_SIZE + GAP_SIZE)
+        if left_over_pixels >= CELL_SIZE:
+            horizontal_cells += 1
+            left_over_pixels -= CELL_SIZE
+        else:
+            left_over_pixels += GAP_SIZE
+        self.map_widgets(horizontal_cells, left_over_pixels)
+
+    def map_widgets(self, horizontal_cells, left_over_pixels):
+        for widget in self.widget_dict:
+            self.grid.remove(widget)
+
+        widget_map = np.zeros((3, horizontal_cells), dtype=int)
+        for widget, config in self.widget_dict.items():
+            x, y = horizontal_cells - config.x - config.width, config.y
+            x_end, y_end = horizontal_cells - config.x, config.y + config.height
+            widget_map[y:y_end, x:x_end] = 1
+            self.grid.attach(widget, x, y, config.width, config.height)
+
+        first_non_empty_index = np.min((widget_map != 0).argmax(axis=1))
+        for n in range(first_non_empty_index):
+            width = CELL_SIZE
+            if n == 0:
+                width += left_over_pixels
+            self.grid.attach(status_widget.LcarswmStatusWidget(width, CELL_SIZE, self.css_provider), n, 0, 1, 1)
+
+        self.grid.show_all()
 
     @staticmethod
     def update_widgets(stop, self):
         while True:
-            for status_widget in self.widget_dict:
-                GLib.idle_add(status_widget.update)
+            for widget in self.widget_dict:
+                GLib.idle_add(widget.update)
 
             if stop():
                 break
