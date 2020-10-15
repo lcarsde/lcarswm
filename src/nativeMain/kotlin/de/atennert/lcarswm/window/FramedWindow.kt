2b0d08d06a33c47446bef8bf12dc9dc6fbfa399b
Andreas Tennert <mail@andreas-tennert.de>
2020-10-15 22:20:13 +0200

    doc: add comments in FramedWindow class

--- src/nativeMain/kotlin/de/atennert/lcarswm/window/FramedWindow.kt
+++ src/nativeMain/kotlin/de/atennert/lcarswm/window/FramedWindow.kt
@@ -6,10 +6,21 @@ import xlib.Window
 
 /**
  * POJO for registered windows.
+ *
+ * @param id Program window ID
+ * @param borderWidth Desired program window border width,
+ *      we need to remember and reset it to this when we stop handling it
  */
 data class FramedWindow(val id: Window, val borderWidth: Int) {
+    /** Frame window ID */
     var frame: Window = 0.convert()
+
+    /** Title bar window ID */
     var titleBar: Window = 0.convert()
+
+    /** Window title (usually depends on program content) */
     var title: String = TextAtomReader.NO_NAME
+
+    /** Name of the program */
     var wmClass: String = TextAtomReader.NO_NAME
 }
