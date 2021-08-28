a2b877edcaeae3b6c2220a6fcee8085d93f71860
Andreas Tennert <mail@andreas-tennert.de>
2021-08-28 19:17:25 +0200

    ci: extract project name from deb script

--- deploy-deb.sh
+++ deploy-deb.sh
@@ -4,36 +4,36 @@ export LC_ALL=en_US.UTF-8
 TIME="$(date '+%a, %d %b %Y %T %z')"
 YEAR="$(date '+%Y')"
 
-echo "preparing debian package build for lcarswm-${CIRCLE_TAG}"
+echo "preparing debian package build for $1-${CIRCLE_TAG}"
 
 # create debian package construction directory
-mkdir -p "build/deb/lcarswm-${CIRCLE_TAG}"
+mkdir -p "build/deb/$1-${CIRCLE_TAG}"
 
 # copy software resources
-cp -r gradle "build/deb/lcarswm-${CIRCLE_TAG}/gradle"
-cp -r src "build/deb/lcarswm-${CIRCLE_TAG}/src"
-cp build.gradle.kts "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp CHANGELOG "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp gradle.properties "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp gradlew "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp LICENSE "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp readme.md "build/deb/lcarswm-${CIRCLE_TAG}/"
-cp settings.gradle.kts "build/deb/lcarswm-${CIRCLE_TAG}/"
+cp -r gradle "build/deb/$1-${CIRCLE_TAG}/gradle"
+cp -r src "build/deb/$1-${CIRCLE_TAG}/src"
+cp build.gradle.kts "build/deb/$1-${CIRCLE_TAG}/"
+cp CHANGELOG "build/deb/$1-${CIRCLE_TAG}/"
+cp gradle.properties "build/deb/$1-${CIRCLE_TAG}/"
+cp gradlew "build/deb/$1-${CIRCLE_TAG}/"
+cp LICENSE "build/deb/$1-${CIRCLE_TAG}/"
+cp readme.md "build/deb/$1-${CIRCLE_TAG}/"
+cp settings.gradle.kts "build/deb/$1-${CIRCLE_TAG}/"
 
 cd "build/deb"
 
 echo "create tarball"
-tar -czf "lcarswm_${CIRCLE_TAG}.orig.tar.gz" "lcarswm-${CIRCLE_TAG}"
-tar -ztf "lcarswm_${CIRCLE_TAG}.orig.tar.gz"
+tar -czf "$1_${CIRCLE_TAG}.orig.tar.gz" "$1-${CIRCLE_TAG}"
+tar -ztf "$1_${CIRCLE_TAG}.orig.tar.gz"
 
 cd ../..
 
 # copy debian packaging files
-cp -r "debian" "build/deb/lcarswm-${CIRCLE_TAG}/debian"
+cp -r "debian" "build/deb/$1-${CIRCLE_TAG}/debian"
 
-echo "building debian package of lcarswm-${CIRCLE_TAG}"
+echo "building debian package of $1-${CIRCLE_TAG}"
 
-cd "build/deb/lcarswm-${CIRCLE_TAG}"
+cd "build/deb/$1-${CIRCLE_TAG}"
 
 sed -i "s/%version%/${CIRCLE_TAG}/" "debian/changelog"
 sed -i "s/%time%/${TIME}/" "debian/changelog"
@@ -44,7 +44,7 @@ debuild -us -uc
 cd ..
 ls -l
 mkdir "deploy"
-mv lcarswm_*.deb deploy/
+mv "$1_*.deb" deploy/
 
 cd ../..
 
