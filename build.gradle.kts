4ea8359196fb7ab73151d94de581821eba35d4ad
Andreas Tennert <mail@andreas-tennert.de>
2022-03-27 21:42:04 +0200

    build: add selection of processor architecture (#19)
    * add support for aarch64 and arm64
    * add changelog entry for architecture selection

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     kotlin("multiplatform") version "1.6.0"
 }
@@ -10,7 +12,27 @@ repositories {
 
 kotlin {
     val nativeTarget = when (System.getProperty("os.name")) {
-        "Linux" -> linuxX64("native")
+        "Linux" -> {
+            val architecture: String = ByteArrayOutputStream().use { os ->
+                project.exec {
+                    commandLine("uname", "-m")
+                    standardOutput = os
+                }
+                os.toString().trim()
+            }
+            when {
+                architecture == "x86_64" -> linuxX64("native")
+                architecture.startsWith("arm64") -> linuxArm64("native")
+                architecture.startsWith("aarch64") -> linuxArm64("native")
+                else -> throw GradleException("Host CPU architecture not supported: $architecture.\n" +
+                        "If you think, it should work, please:\n" +
+                        "1. check your CPU architecture with \"uname -a\",\n" +
+                        "2. find the corresponding target in this list: https://kotlinlang.org/docs/mpp-dsl-reference.html#targets,\n" +
+                        "3. add a it to the architectures in build.gradle.kts,\n" +
+                        "4. run the build and\n" +
+                        "5. if it works, please create a ticket with the changes on https://github.com/lcarsde/lcarswm/issues to have it added permanently.")
+            }
+        }
         else -> throw GradleException("Host OS is not supported.")
     }
 
