bda3c26422a4a0804651f1fac3c9975e0e612968
Andreas Tennert <mail@andreas-tennert.de>
2023-04-10 12:40:50 +0200

    test: rx - fix shareReplay tests

--- src/commonTest/kotlin/de/atennert/rx/operators/ShareReplayTest.kt
+++ src/commonTest/kotlin/de/atennert/rx/operators/ShareReplayTest.kt
@@ -9,29 +9,7 @@ import kotlin.test.assertIs
 
 class ShareReplayTest {
     @Test
-    fun `should replay values`() {
-        val observer = object : Subscriber<Int>() {
-            val received = mutableListOf<Any>()
-            override fun next(value: Int) {
-                received.add(value)
-            }
-        }
-        val subject = Subject<Int>()
-        val obs = subject
-            .apply(shareReplay(2))
-
-        subject.next(1)
-        subject.next(2)
-        subject.next(3)
-
-        obs.subscribe(observer)
-            .unsubscribe()
-
-        assertContentEquals(listOf(2, 3), observer.received)
-    }
-
-    @Test
-    fun `should replay 1 value default`() {
+    fun `should forward values`() {
         val observer = object : Subscriber<Int>() {
             val received = mutableListOf<Any>()
             override fun next(value: Int) {
@@ -43,13 +21,60 @@ class ShareReplayTest {
             .apply(shareReplay())
 
         subject.next(1)
+
+        obs.subscribe(observer)
+
+        subject.next(2)
+
+        assertContentEquals(listOf(2), observer.received)
+    }
+
+    @Test
+    fun `should replay 1 value default`() {
+        val preObserver = object : Subscriber<Int>() {}
+        val observer = object : Subscriber<Int>() {
+            val received = mutableListOf<Any>()
+            override fun next(value: Int) {
+                received.add(value)
+            }
+        }
+        val subject = Subject<Int>()
+        val obs = subject
+            .apply(shareReplay())
+
+        obs.subscribe(preObserver)
+
+        subject.next(1)
         subject.next(2)
         subject.next(3)
 
         obs.subscribe(observer)
-            .unsubscribe()
 
         assertContentEquals(listOf(3), observer.received)
+    }
+
+    @Test
+    fun `should replay 2 values`() {
+        val preObserver = object : Subscriber<Int>() {}
+        val observer = object : Subscriber<Int>() {
+            val received = mutableListOf<Any>()
+            override fun next(value: Int) {
+                received.add(value)
+            }
+        }
+        val subject = Subject<Int>()
+        val obs = subject
+            .apply(shareReplay(2))
+
+        obs.subscribe(preObserver)
+
+        subject.next(1)
+        subject.next(2)
+        subject.next(3)
+
+        obs.subscribe(observer)
+
+        assertContentEquals(listOf(2, 3), observer.received)
     }
 
     @Test
@@ -60,7 +85,7 @@ class ShareReplayTest {
                 received.add(error)
             }
         }
-        (Observable.error<Int>())
+        Observable.error<Int>()
             .apply(map { it + 1 })
             .subscribe(observer)
 
@@ -89,8 +114,12 @@ class ShareReplayTest {
 
     @Test
     fun `should replay and forward complete`() {
+        val preObserver = object : Subscriber<Int>() {}
         val observer = object : Subscriber<Int>() {
             val received = mutableListOf<Any>()
+            override fun next(value: Int) {
+                received.add(value)
+            }
             override fun complete() {
                 received.add("complete")
             }
@@ -98,6 +127,7 @@ class ShareReplayTest {
         val subject = Subject<Int>()
         val obs = subject
             .apply(shareReplay(2))
+        obs.subscribe(preObserver)
 
         subject.next(1)
         subject.next(2)
@@ -105,13 +135,13 @@ class ShareReplayTest {
         subject.complete()
 
         obs.subscribe(observer)
-            .unsubscribe()
 
         assertContentEquals(listOf(2, 3, "complete"), observer.received)
     }
 
     @Test
     fun `should replay values for multiple observers`() {
+        val preObserver = object : Subscriber<Int>() {}
         val observer1 = object : Subscriber<Int>() {
             val received = mutableListOf<Any>()
             override fun next(value: Int) {
@@ -127,6 +157,7 @@ class ShareReplayTest {
         val subject = Subject<Int>()
         val obs = subject
             .apply(shareReplay(2))
+        obs.subscribe(preObserver)
 
         subject.next(1)
         subject.next(2)
@@ -153,12 +184,12 @@ class ShareReplayTest {
         val obs = subject
             .apply(shareReplay(2, true))
 
+        val sub1 = obs.subscribe(observer1)
+        val sub2 = obs.subscribe(observer2)
+
         subject.next(1)
         subject.next(2)
         subject.next(3)
-
-        val sub1 = obs.subscribe(observer1)
-        val sub2 = obs.subscribe(observer2)
 
         sub1.unsubscribe()
         sub2.unsubscribe()
@@ -182,12 +213,12 @@ class ShareReplayTest {
         val obs = subject
             .apply(shareReplay(2))
 
+        val sub1 = obs.subscribe(observer1)
+        val sub2 = obs.subscribe(observer2)
+
         subject.next(1)
         subject.next(2)
         subject.next(3)
-
-        val sub1 = obs.subscribe(observer1)
-        val sub2 = obs.subscribe(observer2)
 
         sub1.unsubscribe()
         sub2.unsubscribe()
